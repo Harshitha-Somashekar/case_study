34acb4fdcae09dcc471dcd7f737d4a2d0efb337c
Harshitha-Somashekar <84902078+Harshitha-Somashekar@users.noreply.github.com>
2021-06-02 11:12:08 +0530

    fixed range

--- Factors.py
+++ Factors.py
@@ -1,6 +1,6 @@
 number = input("Enter the number:")
 factors = []
-for i in range(1,int(number)):
+for i in range(1,int(number) + 1):
     if int(number)%i == 0:
         factors.append(i)
 print("Factors of " + number + " : " + str(factors))
