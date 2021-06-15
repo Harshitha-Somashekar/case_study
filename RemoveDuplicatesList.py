95009f77b7751c7cbe3222712f276b4395f229ca
Harshitha-Somashekar <84902078+Harshitha-Somashekar@users.noreply.github.com>
2021-06-15 11:37:33 +0530

    Update RemoveDuplicatesList.py

--- RemoveDuplicatesList.py
+++ RemoveDuplicatesList.py
@@ -1,7 +1,3 @@
 a = [12,24,35,24,88,120,155,88,120,155]
-a.reverse()
-for i in a:
-    if a.count(i) > 1:
-        a.remove(i)
-a.reverse()
+a = list(dict.fromkeys(a)) #PS: got from google
 print(a)
